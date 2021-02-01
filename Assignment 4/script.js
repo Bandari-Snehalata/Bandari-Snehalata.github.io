245217ba6aff0329d3cb13982d336ef11e12980f
Bandari-Snehalata <snehabandari20@gmail.com>
2021-02-01 10:57:43 +0530

    Update script.js

--- Assignment 4/script.js
+++ Assignment 4/script.js
@@ -9,9 +9,9 @@ names[6]="larry";
 names[7]="paula";
 names[8]="laura";
 names[9]="jim";
+*/
 
-
-for (var i = 0; i < names.length; i++) {
+/*for (var i = 0; i < names.length; i++) {
 	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
         console.log("Goodbye "+ names[i])
 	}
@@ -19,3 +19,15 @@ for (var i = 0; i < names.length; i++) {
 		console.log("Hello "+ names[i])
 	}
 }
+*/
+(function() {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
